fd72ff2bce0660db2d6cc23857626197a8090c71
Carlos Baez <c.baez1223@yahoo.com>
2020-10-05 22:55:27 -0400

    completed 1 suite & 7 tests and all passed

--- calculator/calculator.spec.js
+++ calculator/calculator.spec.js
@@ -1,3 +1,46 @@
 const { add } = require('./calculator.js');
 
-// test away!
+// test('test that truth is truth', () => {
+// 	expect(true).toBe(true);
+// });
+
+// it('should text that true is true', () => {
+// 	expect(true).toBe(true);
+// });
+
+describe('calculator.js', () => {
+	describe('add() tests', () => {
+		it('should return the sum of numbers passed', () => {
+			expect(add(2, 2)).toBe(4);
+			expect(add(2, 3)).toBe(5);
+		});
+
+		it('should return NaN when called with no arguments', () => {
+			const expected = 0;
+			const actual = add();
+			expect(actual).toBe(expected);
+		});
+
+		it('should return 0 when called with no parameters', () => {
+			expect(add()).toBe(0);
+		});
+
+		it('should return the value passed when only one argument is provided', () => {
+			expect(add(1)).toBe(1);
+		});
+
+		it('should return the value passed when only one argument is provided', () => {
+			expect(add(1, null)).toBe(1);
+		});
+
+		it('should handle any number of arguments, seperated by comm', () => {
+			expect(add(1, 2, 3)).toBe(6);
+			expect(add(1, 2, 3, 5)).toBe(11);
+		});
+
+		it('should handle an array of numbers', () => {
+			expect(add(1, 2, 3)).toBe(6);
+			expect(add(1, 2, 3, 5)).toBe(11);
+		});
+	});
+});
